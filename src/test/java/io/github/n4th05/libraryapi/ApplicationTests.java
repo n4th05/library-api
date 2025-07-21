4cf6621c8562b5e9c2ad3c3ffbfd949d4e8870f9
Nathalia Machado <machadonathalia307@gmail.com>
2025-07-21 19:30:55 -0300

    feat: Creating and Configuring the Project

---
+++ src/test/java/io/github/n4th05/libraryapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.n4th05.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
