3dc8e82d2f75903a07819425d114886bb54ea542
Nathalia Machado <machadonathalia307@gmail.com>
2025-08-11 12:07:48 -0300

    feat: Endpoint for "Livro" registration with Bean Validation

--- src/main/java/io/github/n4th05/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/n4th05/libraryapi/controller/LivroController.java
@@ -1,15 +1,37 @@
 package io.github.n4th05.libraryapi.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.n4th05.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.n4th05.libraryapi.controller.dto.ErroResposta;
+import io.github.n4th05.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.n4th05.libraryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/livros")
+@RequestMapping("livros")
 @RequiredArgsConstructor
 public class LivroController {
     
     private final LivroService service;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO dto) {
+        try {
+            // mapear dto para entidade
+            // enviar a entidade para o service validar e salvar na base
+            // criar url para acesso dos dados do livro
+            // retornar codigo created com header location
+            
+            return ResponseEntity.ok(dto);
+        } catch (RegistroDuplicadoException e) {
+            var erroDTO = ErroResposta.conflito(e.getMessage());
+            return ResponseEntity.status(erroDTO.status()).body(erroDTO);
+        }
+    }
 }
--- src/main/java/io/github/n4th05/libraryapi/controller/dto/CadastroLivroDTO.java
+++ src/main/java/io/github/n4th05/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -4,14 +4,25 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.ISBN;
+
 import io.github.n4th05.libraryapi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
 public record CadastroLivroDTO(
+        @ISBN
+        @NotBlank(message = "ISBN Inválido!")
         String isbn,
+        @NotBlank(message = "Campo obrigatório!")
         String titulo,
+        @NotNull(message = "Campo obrigatório!")
+        @Past(message = "Não pode ser uma data futura!")
         LocalDate dataPublicacao,
         GeneroLivro genero,
         BigDecimal preco,
+        @NotNull(message = "Campo obrigatório!")
         UUID idAutor
         ) {
     
