54b44f4bb56511a1c3c5aafbae12943d203a2892
Nathalia Machado <machadonathalia307@gmail.com>
2025-09-08 17:41:51 -0300

    fix: Adjusting SecurityService

--- src/main/java/io/github/n4th05/libraryapi/security/SecurityService.java
+++ src/main/java/io/github/n4th05/libraryapi/security/SecurityService.java
@@ -2,23 +2,21 @@ package io.github.n4th05.libraryapi.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.github.n4th05.libraryapi.model.Usuario;
-import io.github.n4th05.libraryapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class SecurityService {
-
-    private final UsuarioService usuarioService;
     
     public Usuario obterUsuarioLogado() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String login = userDetails.getUsername();
-        return usuarioService.obterPorLogin(login);
+        if(authentication instanceof CustomAuthentication customAuth){
+            return customAuth.getUsuario();
+        }
+
+        return null;
     }
 }
