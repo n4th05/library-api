6e91a3dc15b1b72672e6f4e7f2e286c7cfcd607f
Nathalia Machado <machadonathalia307@gmail.com>
2025-10-07 18:42:45 -0300

    feat: Enabling and testing Swagger Open API in your project

--- src/main/java/io/github/n4th05/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/n4th05/libraryapi/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -50,6 +51,18 @@ public class SecurityConfiguration {
                 .addFilterAfter(jwtCustomAuthenticationFilter, BearerTokenAuthenticationFilter.class) // Adiciona o filtro customizado após o filtro padrão de autenticação por token Bearer.
                 .build(); // Constrói o SecurityFilterChain.
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers( // Ignora a segurança para os endpoints do Swagger UI e do OpenAPI.
+                "/v2/api-docs/**",
+                "/v3/api-docs",
+                "/swagger-resources/**",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/webjars/**"
+            );
+        };
 
     // Configura o prefixo das roles.
      @Bean
